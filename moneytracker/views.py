4407eced1fa5ac007ffa3c4932853e32e71dbcb3
riyas <muhammadriyask11@gmail.com>
2025-08-29 01:01:39 +0530

    feat(transactions): add new transactions page with header included

--- moneytracker/views.py
+++ moneytracker/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def dashboard(request):
     return render(request, "dashboard.html")
+
+
+def transactions(request):
+    return render(request, "transactions.html")
